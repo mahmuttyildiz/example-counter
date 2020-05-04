a515af2636549a67fb180fa9acc4ab6a8393bc9b
Mahmut <mahmutt.yildiz@outlook.com>
2020-05-04 05:07:14 +0300

    counter example finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component{
+  constructor(props){
+      super(props);
+      this.state = {
+          count: 0
+      }
+  }
 
-    </div>
-  );
+  degistir = (operation) => {
+      this.setState({
+          count: operation === "azalt" ? this.state.count - 1 : this.state.count +1
+      })
+  }
+
+  render(){
+      const {count} = this.state;
+      return <div>
+          <h1>{count}</h1>
+          <button onClick={() => {this.degistir("azalt")}}>
+              -
+          </button>
+          <button onClick={() => {this.degistir("artir")}}>
+              +
+          </button>
+      </div>
+  }
 }
 
 export default App;
