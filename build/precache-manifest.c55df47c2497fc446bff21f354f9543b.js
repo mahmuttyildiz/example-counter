051014361355307ba768d94c6e68e66e64c3525e
Mahmut <mahmutt.yildiz@outlook.com>
2020-05-19 17:09:50 +0300

    build features added

---
+++ build/precache-manifest.c55df47c2497fc446bff21f354f9543b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11b51e29737fcd85657eb6c401463b0",
+    "url": "/example-counter/index.html"
+  },
+  {
+    "revision": "a9a7a2c6c2e38475aa17",
+    "url": "/example-counter/static/css/main.340bbdb4.chunk.css"
+  },
+  {
+    "revision": "dfc02f519dcba00cbfbc",
+    "url": "/example-counter/static/js/2.8ee6d4c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/example-counter/static/js/2.8ee6d4c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a7a2c6c2e38475aa17",
+    "url": "/example-counter/static/js/main.6175eb78.chunk.js"
+  },
+  {
+    "revision": "76acb0305e951941c806",
+    "url": "/example-counter/static/js/runtime-main.b7814269.js"
+  }
+]);
